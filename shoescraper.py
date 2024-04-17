1a0446953ff349e55b4bdbeaef44dac726193578
tystratton <tysonthetyrant@gmail.com>
2024-04-17 01:07:33 -0400

    Idek

--- shoescraper.py
+++ shoescraper.py
@@ -36,9 +36,6 @@ def scrape_nike_shoes(url):
 
     # Find all shoe listings on the page
     shoe_listings = soup.find_all('div', class_='product-card')
-    print("hello")
-    print("hello")
-    print("hi?")
     current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # Iterate over each shoe listing and extract relevant information
     for shoe_listing in shoe_listings:
